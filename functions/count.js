a801eb5fd7fd2015a68250fbc5a5e0fd10af85ad
MDShields7 <matthew.duncan.shields@gmail.com>
2020-08-20 15:16:04 -0500

    add counter function

---
+++ functions/count.js
@@ -0,0 +1,11 @@
+var count = 0;
+
+exports.handler = async event => {
+    const subject = event.queryStringParameters.name || 1
+    let num = parseInt(subject)
+    count += num
+    return {
+      statusCode: 200,
+      body: `Count is ${count}!`,
+    }
+  }
