1b4f898496f223d4ad970453989799753d69eaf7
MDShields7 <matthew.duncan.shields@gmail.com>
2020-08-20 15:34:01 -0500

    destructure object

--- functions/count-get.js
+++ functions/count-get.js
@@ -1,15 +1,16 @@
-import count from './count'
+import {count} from './count'
 
 exports.handler = async event => {
-    let keys = Object.keys(count);
-    let values = Object.values(count);
-    let text = '';
-    console.log('hi')
-    for ( let i = 0; i < values.length; i++){
-        text += keys[i] + ': ' + values[i] + '.'
-    }
+    // let keys = Object.keys(count);
+    // let values = Object.values(count);
+    // let text = '';
+    // console.log('hi')
+    // for ( let i = 0; i < values.length; i++){
+    //     text += keys[i] + ': ' + values[i] + '.'
+    // }
     return {
       statusCode: 200,
-      body: `Object is ${text}!`,
+    //   body: `Object is ${text}!`,
+      body: `Count is ${count}!`,
     }
   }
