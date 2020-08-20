9d3748d82b143ae87759bb759c9392c5bb942c32
MDShields7 <matthew.duncan.shields@gmail.com>
2020-08-20 15:31:59 -0500

    logging object

--- functions/count-get.js
+++ functions/count-get.js
@@ -1,8 +1,15 @@
 import count from './count'
 
 exports.handler = async event => {
+    let keys = Object.keys(count);
+    let values = Object.values(count);
+    let text = '';
+    console.log('hi')
+    for ( let i = 0; i < values.length; i++){
+        text += keys[i] + ': ' + values[i] + '.'
+    }
     return {
       statusCode: 200,
-      body: `Count is ${count}!`,
+      body: `Object is ${text}!`,
     }
   }
